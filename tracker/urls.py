9ec76eb37b53ee60ccf31dcb1e861953f7fcf9d2
Roman Peka <pekaromanmail@gmail.com>
2025-08-02 08:32:33 +0300

    feat: create task list view
    - Add TaskListView to display all tasks with pagination.
    - Create task_list.html template to render the table of tasks.
    - Update sidebar navigation with a link to the new page.

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import index
+from .views import index, TaskListView
 
 app_name = "tracker"
 
 urlpatterns = [
     path("", index, name="index"),
+    path("tasks/", TaskListView.as_view(), name="task-list"),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
 from django.http import HttpRequest, HttpResponse
+
+from .models import Task
+
 
 def index(request: HttpRequest) -> HttpResponse:
     return render(request, "tracker/index.html")
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    paginate_by = 10
