bda6141f8bb6b982e0550dd710519b09113ceb2e
Roman Peka <pekaromanmail@gmail.com>
2025-08-10 09:18:38 +0300

    refactor: Prepare application for deployment

---
+++ task_manager/settings/prod.py
@@ -0,0 +1,28 @@
+import os
+
+from .base import *
+
+
+DEBUG = False
+
+ALLOWED_HOSTS = []
+
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['POSTGRES_DB'],
+        'USER': os.environ['POSTGRES_USER'],
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+        'HOST': os.environ['POSTGRES_HOST'],
+        'PORT': int(os.environ['POSTGRES_DB_PORT']),
+    }
+}
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
